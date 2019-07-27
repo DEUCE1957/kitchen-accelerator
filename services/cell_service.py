1e967345cc8f3b90075b6b2f23f8b481f3567c92
alppucodes <albert.iho@tuni.fi>
2019-07-27 14:59:07 +0200

    Added error handling

--- services/cell_service.py
+++ services/cell_service.py
@@ -1,10 +1,11 @@
 from main.models import *
 
 def add_cell(shelf_id):
-    # define a new fridge and add it into existing kitchen
-    new_cell = Cell.objects.create(
-        shelf = shelf.objects.get(id=shelf_id)
-    # save changes to database
-    new_cell.save()
-        
-
+    try:
+        # define a new fridge and add it into existing kitchen
+        new_cell = Cell.objects.create(
+            shelf = shelf.objects.get(id=shelf_id)
+        # save changes to database
+        new_cell.save()
+    except Exception as e:
+        return False
--- services/user_service.py
+++ services/user_service.py
@@ -4,11 +4,18 @@ from main.models import *
 
 def add_user(firstname, lastname, password, email, profilepicture):
     # create a new user with given variables
-    new_user = User.objects.create(
-        first_name=firstname,
-        last_name=lastname,
-        password = make_password(password)
-        email=email,
-        profilepicture=profilepicture)
-    # save changes to database
-    new_user.save()
+    try:
+        new_user = UserProfile.objects.create(
+            first_name=firstname,
+            last_name=lastname,
+            password = make_password(password)
+            email=email,
+            picture=profilepicture)
+        # save changes to database
+        new_user.save()
+        return True
+    except Exception as e:
+        return False
+    
+def delete_user(username):
+    # delete an existing user 
