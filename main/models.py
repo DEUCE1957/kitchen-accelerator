916b798e71ba3376f3d9831918655c865dd71bd1
deuce1957 <2246306w@student.gla.ac.uk>
2019-07-27 13:25:45 +0100

    Debug: Fixed URL mismappings

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ import uuid
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     picture = models.ImageField(upload_to='profile_pics', height_field=None, width_field=None, max_length=64, null=False, default='profile_pics/default.png')
 
