fbc14edecb8082693d1ba88e9d673cb1912a1dd2
deuce1957 <2246306w@student.gla.ac.uk>
2019-07-27 10:50:43 +0100

    Debug: Fixed .gitignore, Add: Sample Logic for Fridge Allocation

--- main/views.py
+++ main/views.py
@@ -15,10 +15,27 @@ def kitchen(request):
 
 
 def kitchens(request):
+    def allocation(shelves, members):
+        total_cells = len(shelves)*16
+        quota = round(total_cells/len(members))
+        member_index, member_num = 0, 0
+        # Allocate to member until member meets quote no. of cells
+        for shelf in shelves:
+            for cell in shelf.cells:
+                cell.owner = members[member_index]
+                member_num += 1
+                if member_num == quota:
+                    member_index += 1
+
     kitchens = Kitchen.objects.order_by('name')
     context_dict = {"kitchens": kitchens}
-    return render(request, 'main/kitchens.html', context = context_dict)
+    return render(request, 'main/kitchens.html', context=context_dict)
 
+def booking(request):
+    def make_booking():
+
+    context_dict = {}
+    return render(request, 'main/about.html', context=context_dict)
 
 def about(request):
     return render(request, 'main/about.html', context={})
