1fb436729eb4b0c4b048925133f65c3800f2e56d
alppucodes <albert.iho@tuni.fi>
2019-07-27 15:34:17 +0200

    Populate users and kitchens

--- populate_database.py
+++ populate_database.py
@@ -5,45 +5,45 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 import django
 django.setup()
 from main.models import *
+from services import user_service, kitchen_service
 
 def populate():
     users = [{"first_name":"John",
               "last_name":"Von Neumann",
-              "password":'',
+              "password":'haha',
               "email":'j.von.neumann@cern.ch',
               "profile_picture":'default.jpg'},
              {"first_name": "Rosalind",
               "last_name": "Franklin",
-              "password": '',
+              "password": 'haha',
               "email": 'rosalind.franklin@cern.ch',
               "profile_picture": 'default.jpg'},
              {"first_name": "Enrico",
               "last_name": "Fermi",
-              "password": '',
+              "password": 'haha',
               "email": 'enrico.fermi@cern.ch',
               "profile_picture": 'default.jpg'},
              {"first_name": "Henrietta",
               "last_name": "Leavitt",
-              "password": '',
+              "password": 'haha',
               "email": 'henrietta.swan.leavitt@cern.ch',
-              "profile_picture": 'default.jpg'},
+              "profile_picture": 'default.jpg'}
              ]
+             
+    for u in users:
+        print(add_user(u))
 
     # define Kitchen-table
     kitchens = [{"location":"Bl38",
-                 "name":"The Ultimate Kitchen",
-                 "fridges":3,
-                 "ovens":2,
-                 "stoves":8},
+                 "name":"The Ultimate Kitchen"},
                 {"location": "Bl500",
-                 "name": "The Shrine",
-                 "fridges": 8,
-                 "ovens": 4,
-                 "stoves": 8},
+                 "name": "The Shrine"},
                 {"location": "Bl10",
-                 "name": "Enter my LEIR",
-                 "fridges": 8,
-                 "ovens": 4,
-                 "stoves": 8},
+                 "name": "Enter my LEIR"}
                 ]
-
+    for k in kitchens:
+        print(add_kitchen(k))
+        
+if __name__ == '__main__':
+    print "Starting Rango population script..."
+    populate()
