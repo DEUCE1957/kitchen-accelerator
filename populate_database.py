7c17d8e7f165a4dddb33f47f727ac7b395187dfe
alppucodes <albert.iho@tuni.fi>
2019-07-27 15:41:32 +0200

    Fixed iterating dictionary

--- populate_database.py
+++ populate_database.py
@@ -10,28 +10,38 @@ from services import user_service, kitchen_service
 def populate():
     users = [{"first_name":"John",
               "last_name":"Von Neumann",
+              "username" : "jonvonneu",
               "password":'haha',
               "email":'j.von.neumann@cern.ch',
               "profile_picture":'default.jpg'},
              {"first_name": "Rosalind",
               "last_name": "Franklin",
+              "username" : "rosafrank",
               "password": 'haha',
               "email": 'rosalind.franklin@cern.ch',
               "profile_picture": 'default.jpg'},
              {"first_name": "Enrico",
               "last_name": "Fermi",
+              "username" : "enriferm",
               "password": 'haha',
               "email": 'enrico.fermi@cern.ch',
               "profile_picture": 'default.jpg'},
              {"first_name": "Henrietta",
               "last_name": "Leavitt",
+              "username" : "henrilea",
               "password": 'haha',
               "email": 'henrietta.swan.leavitt@cern.ch',
               "profile_picture": 'default.jpg'}
              ]
              
     for u in users:
-        print(add_user(u))
+        print(add_user(
+            u["first_name"],
+            u["last_name"],
+            u["username"],
+            u["password"],
+            u["email"],
+            u["profile_picture"]))
 
     # define Kitchen-table
     kitchens = [{"location":"Bl38",
@@ -42,7 +52,9 @@ def populate():
                  "name": "Enter my LEIR"}
                 ]
     for k in kitchens:
-        print(add_kitchen(k))
+        print(add_kitchen(
+        k["location"],
+        k["name"]))
         
 if __name__ == '__main__':
     print "Starting Rango population script..."
