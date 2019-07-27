6f7c1e1387c9b02c2ebc356cc5da81252e0824bb
deuce1957 <2246306w@student.gla.ac.uk>
2019-07-27 13:20:21 +0100

    Debug: Fixed critical issue

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-from django.contribut.auth.models import User
+from django.contribu.auth.models import User
 import uuid
 
 
